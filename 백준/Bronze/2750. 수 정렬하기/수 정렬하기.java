e2659967fd77a470ab49b725ee2d2546aaa056b4
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2023-06-21 15:45:49 +0900

    [Bronze II] Title: 수 정렬하기, Time: 348 ms, Memory: 22472 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++)
+		{
+			arr[i] = s.nextInt();
+		}
+		
+		Arrays.sort(arr);
+	
+		for(int i = 0; i < n; i++)
+			System.out.println(arr[i]);
+			
+	}
+
+}
