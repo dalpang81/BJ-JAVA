4e78ff21ea94a11b647575c72aa1d340b602f5f7
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2023-06-22 17:03:22 +0900

    [Bronze I] Title: 2007년, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,157 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int month = s.nextInt();
+		int date = s.nextInt();
+		
+		if(month == 1)
+		{
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 2)
+		{
+			date = date + 31;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 3)
+		{
+			date = date + 59;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 4)
+		{
+			date = date + 90;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 5)
+		{
+			date = date + 120;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 6)
+		{
+			date = date + 151;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 7)
+		{
+			date = date + 181;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 8)
+		{
+			date = date + 212;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 9)
+		{
+			date = date + 243;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 10)
+		{
+			date = date + 273;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 11)
+		{
+			date = date + 304;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+		
+		if(month == 12)
+		{
+			date = date + 334;
+			if( date % 7 == 0) System.out.println("SUN");
+			if( date % 7 == 1) System.out.println("MON");
+			if( date % 7 == 2) System.out.println("TUE");
+			if( date % 7 == 3) System.out.println("WED");
+			if( date % 7 == 4) System.out.println("THU");
+			if( date % 7 == 5) System.out.println("FRI");
+			if( date % 7 == 6) System.out.println("SAT");
+		}
+			
+	}
+	
+
+}
