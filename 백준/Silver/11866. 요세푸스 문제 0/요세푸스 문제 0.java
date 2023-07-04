a1f8ab0c195e188d839dd50ed80ae14873315507
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2023-07-04 09:13:16 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 280 ms, Memory: 33820 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(System.in);
+		Queue<Integer> q = new LinkedList<>();
+		
+		int n = s.nextInt();
+		int k = s.nextInt();
+		
+		for(int i = 1; i <= n; i++)
+		{
+			q.add(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append('<');
+		
+		while(q.size() > 1) {
+			for(int i = 0; i < k - 1; i++) {
+				int val = q.poll();
+				q.offer(val);
+			}
+			
+			sb.append(q.poll()).append(", ");
+		}
+		
+		sb.append(q.poll()).append('>');
+		System.out.println(sb);
+		
+
+		
+	}
+}
+
