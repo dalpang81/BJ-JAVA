3e980ebe5107e3e45758e8875e7b5da9276db204
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2023-08-02 11:29:01 +0900

    [Gold V] Title: 강의실 배정, Time: 856 ms, Memory: 83664 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        Lecture[] lectures = new Lecture[N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < N; i++) {
+            String[] sarr = br.readLine().split(" ");
+            int s = Integer.parseInt(sarr[0]);
+            int e = Integer.parseInt(sarr[1]);
+
+            lectures[i] = new Lecture(s, e);
+        }
+        Arrays.sort(lectures);
+
+        pq.offer(lectures[0].end);
+
+        for(int i = 1; i < N; i++) {
+            if(lectures[i].start >= pq.peek())
+                pq.poll();
+
+            pq.offer(lectures[i].end);
+        }
+
+        bw.write(pq.size() + "\n");
+        bw.flush();
+    }
+
+    static class Lecture implements Comparable<Lecture> {
+        int start, end;
+
+        public Lecture(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Lecture o) {
+            if(start == o.start)
+                return end - o.end;
+            return start - o.start;
+        }
+    }
+
+}
