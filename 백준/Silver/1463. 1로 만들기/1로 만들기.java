689e98d3878f30135424e580af021213cff62402
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2024-05-29 16:02:13 +0900

    [Silver III] Title: 1로 만들기, Time: 248 ms, Memory: 72696 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,42 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	static Integer[] dp;
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		dp = new Integer[N + 1];
-		dp[0] = dp[1] = 0;
- 
-		System.out.print(recur(N));
- 
-	}
- 
-	static int recur(int N) {
- 
-		if (dp[N] == null) {
-			// 6으로 나눠지는 경우 
-			if (N % 6 == 0) {
-				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-			}
-			// 3으로만 나눠지는 경우 
-			else if (N % 3 == 0) {
-				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-			}
-			// 2로만 나눠지는 경우 
-			else if (N % 2 == 0) {
-				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-			}
-			// 2와 3으로 나누어지지 않는 경우
-			else {
-				dp[N] = recur(N - 1) + 1;
-			}
-		}
-		return dp[N];
-	}
- 
+
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new Integer[N + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(makeOne(N));
+    }
+
+    static int makeOne(int num) {
+        if(dp[num] == null) {
+            if(num % 6 == 0)
+                dp[num] = Math.min(makeOne(num - 1), Math.min(makeOne(num / 3), makeOne(num / 2))) + 1;
+
+            else if(num % 3 == 0)
+                dp[num] = Math.min(makeOne(num / 3), makeOne(num - 1)) + 1;
+
+            else if(num % 2 == 0)
+                dp[num] = Math.min(makeOne(num / 2), makeOne(num - 1)) + 1;
+
+            else
+                dp[num] = makeOne(num - 1) + 1;
+        }
+
+        return dp[num];
+    }
 }
