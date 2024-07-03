d7801ed1ddc889f0af1d297fbdf5762641d230a3
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2024-07-03 15:05:12 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1928 ms, Memory: 90496 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        String S = br.readLine();
+
+        BigInteger N = new BigInteger(S, 2);
+        String result = N.toString(8);
+        System.out.println(result);
+    }
+
+}
