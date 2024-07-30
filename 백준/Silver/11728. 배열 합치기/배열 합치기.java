d71091e846b50293ae1432d89ef21ff1616009ed
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2024-07-30 13:49:17 +0900

    [Silver V] Title: 배열 합치기, Time: 1684 ms, Memory: 314432 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -11,25 +11,39 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N + M];
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[M];
 
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
-            arr[i + N] = Integer.parseInt(st.nextToken());
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
-
+        int p1 = 0, p2 = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i : arr) {
-            sb.append(i + " ");
+
+        while(p1 < N && p2 < M) {
+            if(arr1[p1] <= arr2[p2])
+                sb.append(arr1[p1++] + " ");
+            else
+                sb.append(arr2[p2++] + " ");
         }
+
+        if(p1 == N) {
+            for(int i = p2; i < M; i++)
+                sb.append(arr2[i] + " ");
+        }
+        if (p2 == M) {
+            for(int i = p1; i < N; i++)
+                sb.append(arr1[i] + " ");
+        }
+
         System.out.println(sb);
 
 
