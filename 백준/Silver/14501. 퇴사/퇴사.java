5c5e88be1beb5cae744b8758a84c8178c8796ba4
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2024-08-29 13:54:32 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] T = new int[N];
+        int[] P = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        for(int i = 0; i < N; i++) {
+            if(i + T[i] <= N) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        System.out.println(dp[N]);
+
+
+    }
+}
