f35b97bed22e23e7fd121797bd8ca57f682e0968
Dal_Peng <48942037+dalpang81@users.noreply.github.com>
2024-08-30 13:32:19 +0900

    [Bronze V] Title: 새로운 시작, Time: 88 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        System.out.println("파이팅!!");
+
+        
+
+
+    }
+}
